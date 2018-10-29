07d0fa23dc3e4f15c575610f9a627e7248d795bc
Dejmi89 <sticktothecode89@gmail.com>
2018-10-29 22:42:06 +0100

    stage one

---
+++ my-app/src/routes.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Layout from './High Order Components/Layout'
+
+const Routes = (props) => {
+  return(
+    <Layout>
+      Second line
+    </Layout>
+  )
+}
+
+export default Routes;
